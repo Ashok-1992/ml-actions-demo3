b28c0d3774bb8c7fcefcefd61c229312312da073
Ashok-1992 <ashokchan92@gmail.com>
2026-04-24 16:18:04 +0530

    1_Remove unnecessary blank lines in train.py

--- train.py
+++ train.py
@@ -1,11 +1,8 @@
 print(" ML code running inside GitHub Actions")
-
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 X, y = load_iris(return_X_y=True)
-
 model = RandomForestClassifier()
 model.fit(X, y)
-
 print("Model trained successfully")
 print("Accuracy:", model.score(X, y))
